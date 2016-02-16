998f1f6c034c27b246f66465e5e8ede24397199d
C1one_38 <c1one@yandex.ru>
2016-02-16 22:46:14 +0300

    First homework

--- Lesson-1/index.js
+++ Lesson-1/index.js
@@ -1,4 +1,19 @@
-var ansi = require('ansi');
-var cursor = ansi(process.stdout);
-cursor.beep();
-console.log('Hello world!');
+var random = require('random-gen');
+var prompt = require('prompt');
+var cc = require('curly-colors');
+
+console.log(cc('<{yellow>I think of a number between 1 and 3. Try to gues it!<}>'));
+
+random.mixin('myNum' ,['1', '2', '3']);
+var myNumber = random.myNum(1);
+
+prompt.start();
+
+prompt.get(['Number'], function(err, result) {
+  if (result.Number === myNumber) {
+    console.log(cc('<{green>You win!<}>'));
+  }
+  else {
+    console.log(cc('<{red>You are wrong! My number was ' + myNumber + '<}>'));
+  }
+});
